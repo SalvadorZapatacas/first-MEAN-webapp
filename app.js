8f9d78990f1a2ad441036acbf8dc400eefa2ef01
Salvador Zapata <salvazapata@outlook.com>
2017-07-11 18:23:51 +0200

    Configurado cabeceras

--- app.js
+++ app.js
@@ -20,7 +20,19 @@ app.use(bodyParser.json());
 
 
 //Configuramos cabeceras http
+//Creamos un middleware
+app.use((req, res, next) => {
+    //Permitimos el acceso a todos los dominios
+    res.header('Access-Control-Allow-Origin', '*');
 
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+
+    res.header('Access-Control-Allow-Methods' , 'GET, POST, OPTIONS, PUT, DELETE');
+
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 //rutas base
 app.use('/api', user_routes);
