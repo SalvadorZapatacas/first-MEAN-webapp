c3312d7de29dced127017073618dc0094499d044
Salvador Zapata <salvazapata@outlook.com>
2017-07-11 13:58:05 +0200

    Implementado método  getAlbum del controlador album

--- controllers/album.js
+++ controllers/album.js
@@ -17,7 +17,31 @@ var Song = require('../models/song');
 //Métodos
 
 function getAlbum(req, res){
-    res.status(200).send({message : 'Acción getAlbum'});
+    var albumId = req.params.id;
+
+    /**
+     * Explicación del método populate()
+     * Sirve como para hacer un 'join' en mongoose
+     * Entonces carga los datos asociados del artista en el Album
+     * El path sería la propiedad donde se van a cargar los datos
+     * del objeto asociado a esta propiedad, en este caso 'artist',
+     * nos cargará un objeto de tipo artista con todos los datos
+     * 
+     * En RESUMEN, conseguimos todos los datos del artista que 
+     * ha creado el album
+     */
+
+    Album.findById(albumId).populate({path: 'artist'}).exec((err, album)=>{
+        if(err){
+            res.status(500).send({message : 'Error en la petición'});
+        }else{
+            if(!album){
+                res.status(404).send({message : 'No existe el album'});
+            }else{
+                res.status(200).send({album});
+            }
+        }
+    });
 }
 
 
@@ -47,10 +71,7 @@ function saveAlbum(req, res){
     });
 
 
-
-
 }
-
 
 
 
--- routes/album.js
+++ routes/album.js
@@ -18,7 +18,7 @@ var md_auth = require('../middlewares/authenticated');
 var md_upload = multipart({ uploadDir: './uploads/album'});
 
 
-api.get('/album/', md_auth.ensureAuth , AlbumController.getAlbum);
+api.get('/album/:id', md_auth.ensureAuth , AlbumController.getAlbum);
 api.post('/album/', md_auth.ensureAuth , AlbumController.saveAlbum);
 
 
