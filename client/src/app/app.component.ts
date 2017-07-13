52fa7c5a05618fd150ecc87dcc7690cb12f5a4bb
Salvador Zapata <salvazapata@outlook.com>
2017-07-13 13:01:11 +0200

    Entendiendo relación entre vistas y componentes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+//Importamos modelo usuario
+import { User } from './models/user';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'MUSIFY!';
+  public title = 'MUSIFY!';
+  public user : User;
+
+  //Aquí van los datos del usuario logueado
+  public identity;
+  
+  public token;
+
+  // Asigna valor a las propiedades ( un constructor vaya ...)
+  //Más adelante mediante formulario se irán metiendo valores
+  constructor(){
+    this.user = new User('','','','','','ROLE_USER','');
+
+  }
+
 }
